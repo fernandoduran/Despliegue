28885f56d32ec7269deb2141a53aca5255b8d120
fernandoduran <fduranruiz@gmail.com>
2017-02-06 20:23:03 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Prueba de Despliegue desde GitHub</title>
+	</head>
+	<body>
+		<h1>Despliegue desde GitHub</h1>
+		<?php
+			phpinfo();
+		?>
+	</body>
+</html>
